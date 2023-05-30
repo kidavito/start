6fb0fc9b5be1abcbd82a39b4f9d79aea7e9c2cf5
kidavito <kidavito.user@gmail.com>
2023-05-31 01:52:43 +0700

    paragraph updated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Updated v4!</p>
+        <p>Updated v6!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
